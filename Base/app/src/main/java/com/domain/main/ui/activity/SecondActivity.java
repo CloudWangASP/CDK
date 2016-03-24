998163f3fa0ded8fbdeb390ea6de75ad8c51924c
CloudWangASP <cloud_318@163.com>
2016-03-24 13:09:44 +0800

    add RecyclerView

--- Base/app/src/main/java/com/domain/main/ui/activity/SecondActivity.java
+++ Base/app/src/main/java/com/domain/main/ui/activity/SecondActivity.java
@@ -1,34 +1,60 @@
 package com.domain.main.ui.activity;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
 import com.domain.main.R;
+import com.domain.main.ui.adapter.GalleryAdapter;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
-@EActivity
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@EActivity(R.layout.activity_second)
 public class SecondActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    @ViewById
+    RecyclerView recyclerView;
+    private List<String> mDatas;
+    private GalleryAdapter mAdapter;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    @AfterViews
+    void initView() {
+        initDatas();
+        //设置布局管理器
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        //设置适配器
+        mAdapter = new GalleryAdapter(this, mDatas);
+        mAdapter.setOnItemClickLitener(new GalleryAdapter.OnItemClickLitener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(View view, int position) {
+                Toast.makeText(SecondActivity.this, position + "", Toast.LENGTH_SHORT)
+                        .show();
             }
         });
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    private void initDatas() {
+        mDatas = new ArrayList<>(Arrays.asList("http://b394.photo.store.qq.com/psb?/V10IHpOG3mfnbo/uWrDO42KCowcfYDYXgtDThwwCaxDgJQT2FnQKV*ANrc!/b/dIoBAAAAAAAA&bo=IAPCASADwgEFByQ!&rf=viewer_4",
+                "http://b374.photo.store.qq.com/psb?/V10IHpOG3mfnbo/eS9Lh1N24MTNotXtg8uqwk9JcGDmBquX2OFP51L*.ms!/b/dHYBAAAAAAAA&bo=MASAAj8GuwMFABQ!&rf=viewer_4",
+                "http://b110.photo.store.qq.com/psb?/V10IHpOG3mfnbo/cNVZua05svLdY4uy0LmZOkDiAXxoC8trhtqHCYPd3T0!/b/dG4AAAAAAAAA&bo=5wOAAkAGAQQFAAY!&rf=viewer_4",
+                "http://b374.photo.store.qq.com/psb?/V10IHpOG3mfnbo/gxadtR6wZhk4TGJgzXnkj5IQj3ksCn.61hcvk*pDnHM!/b/dHYBAAAAAAAA&bo=cgSAAkAGhAMFABY!&rf=viewer_4",
+                "http://b394.photo.store.qq.com/psb?/V10IHpOG3mfnbo/60eI.X*QUNZ4yvKMBqoDyzK5tFriP7H*TDWgmsCn3GE!/b/dIoBAAAAAAAA&bo=FQIgAxUCIAMFACM!&rf=viewer_4",
+                "http://b32.photo.store.qq.com/psb?/V10IHpOG3mfnbo/KXjSdx8qrOThUF3uhTlg8UGoKM24eH59AWUOLuD.JzU!/b/dCAAAAAAAAAA&bo=cgSAAkAGhAMFABY!&rf=viewer_4",
+                "http://b32.photo.store.qq.com/psb?/V10IHpOG3mfnbo/FzxwSUL5CfYtskn9MgtHA0PxTMHXnm*MW2jgR7djets!/b/dCAAAAAAAAAA&bo=cgSAAkAGhAMFABY!&rf=viewer_4",
+                "http://b22.photo.store.qq.com/psb?/V10IHpOG3mfnbo/Zs9dJN*.w7sKCirGjc3kK*q5uSqoXmXTFEP0QLTlmSg!/b/dBYAAAAAAAAA&bo=cgSAAkAGhAMFABY!&rf=viewer_4",
+                "http://b99.photo.store.qq.com/psb?/V10IHpOG3mfnbo/h4UOvSSo2RCfNIdfL2Jcv1QGwrFRaXJSxplBeJAFP7Y!/b/dGMAAAAAAAAA&bo=cgSAAgAAAAAFANc!&rf=viewer_4",
+                "http://b351.photo.store.qq.com/psb?/V10IHpOG3mfnbo/cQDduxkhVxDYIjQo03LnDOx.koFJqMw8ncNdqSBeaoU!/b/dGSKOdEgFAAA&bo=TwOAAgAAAAADAOs!&rf=viewer_4"));
     }
 
 }
